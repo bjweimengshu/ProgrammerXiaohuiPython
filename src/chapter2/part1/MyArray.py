04842a86f73e7cf45df8a5320ca6486849b65eba
bjweimengshu <bjweimengshu@163.com>
2020-04-15 11:03:12 +0800

    修改数组插入删除元素的bug

--- src/chapter2/part1/MyArray.py
+++ src/chapter2/part1/MyArray.py
@@ -9,7 +9,7 @@ class MyArray:
         if index < 0 or index > self.size:
             raise Exception("超出数组实际元素范围！")
         # 从右向左循环，逐个元素向右挪一位。
-        for i in range(self.size-1, -1, -1):
+        for i in range(self.size-1, index-1, -1):
             self.array[i+1] = self.array[i]
         # 腾出的位置放入新元素
         self.array[index] = element
@@ -23,7 +23,7 @@ class MyArray:
         if self.size >= len(self.array):
             self.resize()
         # 从右向左循环，逐个元素向右挪一位。
-        for i in range(self.size-1, -1, -1):
+        for i in range(self.size-1, index-1, -1):
             self.array[i+1] = self.array[i]
         # 腾出的位置放入新元素
         self.array[index] = element
@@ -41,7 +41,7 @@ class MyArray:
         if index < 0 or index >= self.size:
             raise Exception("超出数组实际元素范围！")
         # 从左到右，逐个元素向左挪动一位
-        for i in range(index, self.size):
+        for i in range(index, self.size-1):
             self.array[i] = self.array[i+1]
         self.size -= 1
 
@@ -52,9 +52,14 @@ class MyArray:
 
 array = MyArray(4)
 array.insert_v2(0, 10)
-array.insert_v2(0, 11)
-array.insert_v2(0, 15)
-array.remove(1)
+array.insert_v2(1, 11)
+array.insert_v2(2, 12)
+array.insert_v2(3, 13)
+array.insert_v2(4, 14)
+array.insert_v2(2, 15)
+array.insert_v2(2, 16)
+array.insert_v2(2, 17)
+array.remove(0)
 array.output()
 
 
